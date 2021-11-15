34173374f4d4a0bb0c00a1df26c7404b942c1d97
Shreyas Bhaktharam <bhaktharamshreyas@gmail.com>
2021-11-15 07:03:52 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import Flask, request
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import Moves
+
+
+db.init_app(app)
+db.create_all()
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,33 @@
+from app import db
+
+
+class Moves(db.Model):
+    __tablename__ = 'moves'
+
+    MoveId = db.Column(db.Integer, primary_key=True, unique=True)
+    MoveName = db.Column(db.String(20), nullable=False)
+    effects = db.Column(db.String(20))
+    damage = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(50))
+    stat_changes = db.Column(db.String(20))
+
+    def __init__(self, MoveId, MoveName, effects, damage, description, stat_changes):
+        self.MoveId = MoveId
+        self.MoveName = MoveName
+        self.effects = effects
+        self.damage = damage
+        self.description = description
+        self.stat_changes = stat_changes
+
+    def __repr__(self):
+        return '<Pokemon %r>' % self.name
+
+    def serialize(self):
+        return {
+            'MoveId': self.MoveId,
+            'MoveName': self.MoveName,
+            'effects': self.effects,
+            'damage': self.damage,
+            'description': self.description,
+            'stat_changes': self.stat_changes
+        }
